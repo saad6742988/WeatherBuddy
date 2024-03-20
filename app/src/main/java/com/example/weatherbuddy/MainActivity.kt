cffe52d8e3c10468e3b4f92461f241efb6f4a36a
saad6742988 <ms6742988@gmail.com>
2024-03-20 14:34:43 +0100

    Added Splash Screen With Navigation Controller

--- app/src/main/java/com/example/weatherbuddy/MainActivity.kt
+++ app/src/main/java/com/example/weatherbuddy/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherbuddy.navigation.SplashNavigation
 import com.example.weatherbuddy.ui.splashScreen.SplashScreen
 import com.example.weatherbuddy.ui.theme.WeatherBuddyTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,25 +21,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 //                    Greeting("Android")
-                    SplashScreen()
+                    SplashNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherBuddyTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/weatherbuddy/ui/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/weatherbuddy/ui/splashScreen/SplashScreen.kt
@@ -1,25 +1,65 @@
 package com.example.weatherbuddy.ui.splashScreen
 
-import android.window.SplashScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.weatherbuddy.R
 import com.example.weatherbuddy.viewModels.SplashViewModel
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen()
+fun SplashScreen(changeToMain: () -> Unit)
 {
+
+    LaunchedEffect(key1 = true) {
+        delay(4000)
+        changeToMain()
+    }
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.splash_animation
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
     val splashViewModel:SplashViewModel= hiltViewModel()
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = splashViewModel.test())
+
+
+
+        LottieAnimation(
+            composition = preloaderLottieComposition,
+            progress = preloaderProgress,
+            modifier = Modifier.size(150.dp)
+        )
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreenPrev()
+{
+    SplashScreen(changeToMain = {})
 }
