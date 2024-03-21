a86cbfdce45e7abd8d0df66170d6f28b9c8fc5bd
saad6742988 <ms6742988@gmail.com>
2024-03-21 15:23:57 +0100

    Added Background On main screen

--- app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
@@ -1,21 +1,36 @@
 package com.example.weatherbuddy.ui.mainScreen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.weatherbuddy.R
+
 
 @Composable
 fun MainScreen()
 {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+    val viewModel:MainViewModel = hiltViewModel()
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.sunny_background),
+                contentScale = ContentScale.FillWidth
+            )
     ) {
-        Text(text = "Main")
+
     }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun MainScreenPrev()
+{
+    MainScreen()
 }
