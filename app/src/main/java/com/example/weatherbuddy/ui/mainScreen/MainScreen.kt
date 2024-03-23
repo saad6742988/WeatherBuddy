2b0d7364a56487c9830f514d4a889c6d5bba65b2
saad6742988 <ms6742988@gmail.com>
2024-03-23 16:17:13 +0100

    Added SearchBar

--- app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
@@ -1,12 +1,26 @@
 package com.example.weatherbuddy.ui.mainScreen
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.weatherbuddy.R
@@ -16,6 +30,12 @@ import com.example.weatherbuddy.R
 fun MainScreen()
 {
     val viewModel:MainViewModel = hiltViewModel()
+    MainScreenLayout()
+}
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreenLayout()
+{
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
@@ -24,7 +44,37 @@ fun MainScreen()
                 contentScale = ContentScale.FillWidth
             )
     ) {
-
+        val (searchBar) = createRefs()
+        SearchBar(
+            query = "",
+            onQueryChange = {},
+            onSearch = {},
+            active = false,
+            onActiveChange = {},
+            placeholder = {
+                Text(text = "Search City")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    contentDescription = null,
+                    modifier = Modifier.padding(top = 0.dp)
+                )
+            },
+            tonalElevation = 0.dp,
+            modifier = Modifier.constrainAs(searchBar){
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+            }
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(56.dp)
+                .clip(RoundedCornerShape(150.dp))
+        ) {
+            
+        }
     }
 }
 
@@ -32,5 +82,5 @@ fun MainScreen()
 @Preview(showBackground = true, showSystemUi = true)
 fun MainScreenPrev()
 {
-    MainScreen()
+    MainScreenLayout()
 }
