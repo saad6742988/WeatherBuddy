c23526ab044c8f30f8079bd3b53b5f3e1c52308f
saad6742988 <ms6742988@gmail.com>
2024-03-27 15:37:28 +0100

    Add WeatherConditionAnimation, WeatherTemperatureDetails, DayAndDate

--- app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/weatherbuddy/ui/mainScreen/MainScreen.kt
@@ -1,22 +1,33 @@
 package com.example.weatherbuddy.ui.mainScreen
 
+import android.text.Layout
 import android.util.Log
+import android.widget.GridLayout
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,18 +36,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.weatherbuddy.R
 import kotlin.reflect.KFunction1
 
@@ -78,7 +100,7 @@ fun MainScreenLayout(
                 contentScale = ContentScale.FillWidth
             )
     ) {
-        val (searchBar,cityName) = createRefs()
+        val (searchBar,cityName,weatherDetails) = createRefs()
         SearchBar(
             searchText,
             isSearching,
@@ -98,6 +120,14 @@ fun MainScreenLayout(
             start.linkTo(parent.start)
             end.linkTo(parent.end)
         })
+        WeatherDetails(
+            modifier = Modifier.constrainAs(weatherDetails){
+                top.linkTo(cityName.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                height = Dimension.wrapContent
+            }
+        )
     }
 }
 
@@ -143,7 +173,8 @@ fun SearchBar(
                     Text(
                         text = city.toString(),
                         fontSize = 18.sp,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
                             .clickable {
                                 Log.d("SearchBar", "SearchBar: $city")
                                 setSelectedCity(city.toString())
@@ -152,7 +183,8 @@ fun SearchBar(
                             .padding(vertical = 4.dp, horizontal = 8.dp)
                     )
                     Spacer(
-                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 4.dp)
                             .fillMaxWidth()
                             .height(1.dp)
                             .background(Color.LightGray)
@@ -167,7 +199,10 @@ fun CityName(selectedCity: State<String>, modifier: Modifier = Modifier)
 {
     Row(
         horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
         Icon(
             imageVector = Icons.Outlined.LocationOn,
@@ -177,6 +212,234 @@ fun CityName(selectedCity: State<String>, modifier: Modifier = Modifier)
             text = selectedCity.value,
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
+        )
+    }
+}
+@Composable
+fun WeatherDetails(modifier: Modifier = Modifier)
+{
+    Column(
+        modifier = modifier.padding(horizontal = 12.dp)
+    ) {
+        Row (
+            modifier= Modifier
+                .weight(0.5F)
+                .padding(top = 8.dp)
+        ){
+            MainWeatherDetails()
+        }
+
+        Row(
+            modifier= Modifier
+                .weight(0.3F)
+                .padding(vertical = 8.dp)
+        ) {
+            DayAndDate()
+        }
+        Row(
+            modifier= Modifier
+                .weight(1F)
+                .padding(bottom = 10.dp)
+        ) {
+            OtherWeatherDetails()
+        }
+    }
+}
+@Composable
+fun MainWeatherDetails()
+{
+    Row() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.8F)
+        ){
+            //Render Lottie Animations for weather
+            WeatherConditionAnimation()
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1F)
+        ){
+            //Weather temperature details
+            WeatherTemperatureDetails()
+        }
+    }
+}
+@Composable
+fun WeatherConditionAnimation()
+{
+    val preloaderWeatherAnimComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.splash_animation
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderWeatherAnimComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+    LottieAnimation(
+        composition = preloaderWeatherAnimComposition,
+        progress = preloaderProgress,
+        modifier = Modifier.size(150.dp)
+    )
+    Text(
+        text = "Sunny",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.ExtraBold
+    )
+}
+@Composable
+fun WeatherTemperatureDetails()
+{
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+                .weight(1F)
+        ) {
+            //Main Temperature Data
+            Text(
+                text = "31"+"",
+                fontSize = 64.sp,
+                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.End,
+//                modifier = Modifier.weight(1F)
+            )
+            Text(
+                text = "°C",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.SemiBold,
+//                modifier = Modifier.weight(0.6F)
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 12.dp)
+                .weight(0.6F)
+        )  {
+            // Min Max Temperature Data
+            Column {
+                Text(
+                    text = "Min: "+"29"+"°C",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = "Max: "+"29"+"°C",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+
+        }
+    }
+}
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun OtherWeatherDetails()
+{
+    Card(
+        shape = CardDefaults.outlinedShape,
+        border = BorderStroke(1.dp, Color.White),
+        colors = CardDefaults.outlinedCardColors(containerColor = Color(160,160,160,50)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp)
+    ) {
+        FlowRow(
+            horizontalArrangement = Arrangement.SpaceAround,
+            maxItemsInEachRow = 3,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 4.dp)
+        ) {
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+            OtherDetailsCard(
+                name = "Humidity",
+                icon = Icons.Outlined.Home,
+                value = "70",
+            )
+        }
+    }
+}
+@Composable
+fun OtherDetailsCard(name: String, icon: ImageVector, value: String)
+{
+    Box(
+        modifier = Modifier.padding(vertical = 10.dp)
+    ){
+        Card(
+            shape = CardDefaults.outlinedShape,
+            border = BorderStroke(1.dp, Color.White),
+            colors = CardDefaults.outlinedCardColors(containerColor = Color(160, 160, 160, 50)),
+
+            modifier = Modifier
+                .size(100.dp)
+                .padding(vertical = 0.dp)
+        ) {
+
+        }
+    }
+}
+@Composable
+fun DayAndDate()
+{
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Tuesday",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "27 March 2024",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold
         )
     }
 }
@@ -196,4 +459,14 @@ fun MainScreenPrev()
         viewModel::setSelectedCity,
         viewModel.selectedCity.collectAsState()
     )
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true, wallpaper = Wallpapers.NONE)
+fun CardPrev()
+{
+   OtherDetailsCard(
+       name = "Humidity",
+       icon = Icons.Outlined.Home,
+       value = "70")
 }
