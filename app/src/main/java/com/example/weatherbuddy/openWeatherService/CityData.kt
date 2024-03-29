f7baa3b885d97d9574c8c8df279ef9f93e7cdcde
saad6742988 <ms6742988@gmail.com>
2024-03-29 22:05:29 +0100

    Add OpenWeatherGeoCodingService using Retrofit

---
+++ app/src/main/java/com/example/weatherbuddy/openWeatherService/CityData.kt
@@ -0,0 +1,16 @@
+package com.example.weatherbuddy.openWeatherService
+
+import com.google.gson.annotations.SerializedName
+
+data class CityData(
+    @SerializedName("country")
+    val country: String,
+    @SerializedName("lat")
+    val lat: Double,
+    @SerializedName("lon")
+    val lon: Double,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("state")
+    val state: String
+)
